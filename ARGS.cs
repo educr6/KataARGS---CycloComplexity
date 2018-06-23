dd2a58b1b4d0d52cfee3fc2af075d4e37b8f1b11
educr6 <educr6@gmail.com>
2018-06-23 05:21:46 -0400

    Update ARGS.cs

--- ARGS.cs
+++ ARGS.cs
@@ -6,38 +6,60 @@ using System.Threading.Tasks;
 
 namespace KataARGS
 {
-   public class ARGS
+    public class ARGS
     {
-        public bool hasLogin;
-        public bool hasPort;
+        public bool hasLogin = false;
+        public bool hasPort = false;
         int port;
-        public bool hasDirectory;
+        public bool hasDirectory = false;
+
+
+        public void hasPortMethod(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i] == "-p")
+                {
+                    if (i + 1 < args.Length)
+                        hasPort = int.TryParse(args[i + 1], out port);
+
+                }
+
+
+            }
+        }
+
+        public void hasDirectoryMethod(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                
+                if (args[i] == "-d")
+                {
+                    if (i + 1 < args.Length)
+                    {
+                        if (args[i + 1] != "-p")
+                            hasDirectory = true;
+                    }
+
+                }
+
+
+            }
+
+        }
 
         public void main(string[] args)
         {
-            hasLogin = false;
-            hasPort = false;
-            hasDirectory = false;
+           
 
             if (args.Length > 0)
             {
                 hasLogin = args[0] == "-l";
             }
 
-            for (int i = 0; i < args.Length; i++)
-            {
-                if (args[i] == "-p" && (i + 1 < args.Length)) {
-                    hasPort = int.TryParse(args[i + 1],out port);
-
-                }
-                if (args[i] == "-d" && (i + 1 < args.Length))
-                {
-                    if (args[i + 1] != "-p")
-                        hasDirectory = true;
-                }
-                    
-                    
-            }
+            hasDirectoryMethod(args);
+            hasPortMethod(args);
            
 
         }
